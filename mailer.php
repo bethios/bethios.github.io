c9b1fd0f7d977de9e9cc530cadf77a727d902e6f
Beth Cummings <bethios@gmail.com>
2017-08-21 16:28:09 -0500

    php

--- mailer.php
+++ mailer.php
@@ -1,54 +1,38 @@
 <?php
-    // My modifications to mailer script from:
-    // http://blog.teamtreehouse.com/create-ajax-contact-form
-    // Added input sanitizing to prevent injection
+    function spamcheck($field)
+    {
+        //filter_var() sanitizes the e-mail
+        //address using FILTER_SANITIZE_EMAIL
+        $field=filter_var($field, FILTER_SANITIZE_EMAIL);
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
-
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-            exit;
+        //filter_var() validates the e-mail
+        //address using FILTER_VALIDATE_EMAIL
+        if(filter_var($field, FILTER_VALIDATE_EMAIL))
+        {
+            return true;
         }
-
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
-
-        // Set the email subject.
-        $subject = "New contact from $name";
-
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
-
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+        else
+        {
+            return false;
         }
+    }
 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+    function sendMail($toEmail, $fromEmail, $subject, $message)
+    {
+        $validFromEmail = spamcheck($fromEmail);
+        if($validFromEmail)
+        {
+            mail($toEmail, $subject, $message, "From: $fromEmail");
+        }
+    }
+    $email = isset($_POST['emailInput']) ? $_POST['emailInput'] : false;
+
+    if($email != false)
+    {
+        $yourEmail = "example@example.com";.
+        $subject = "Link";
+        $message = "The link and some message";
+        $success = sendMail($email, $yourEmail, $subject, $message);
     }
 
 ?>
