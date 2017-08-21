75f088bbf35024c65f00fcb4a953cfbe2baa3a03
Beth Cummings <bethios@gmail.com>
2017-08-21 13:54:59 -0500

    php fix

--- scripts/contact.js
+++ scripts/contact.js
@@ -4,9 +4,6 @@ $(function () {
     // validator files are included in the download package
     // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-    //$('#contact-form').validator();
-
-
     // when the form is submitted
     $('#contact-form').on('submit', function (e) {
 
